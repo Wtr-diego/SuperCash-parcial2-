dcdec0fbcc5a97caadc73444decc0e790f8d81f1
leslenaaa <vh3081001@gmail.com>
2026-04-18 07:31:01 -1200

    se agregaron modulos a la clase

--- EL/auditoria.cs
+++ EL/auditoria.cs
@@ -9,7 +9,9 @@ namespace EL
         public DateTime FechaHora { get; set; }
         public string Accion { get; set; } // "Eliminó Producto", "Inició Sesión", etc.
         public string Detalle { get; set; }
+        public string Modulo { get; set; } // "Inventario", "Ventas", "Login"
     }
 }
 
 // se hicieron correciones por un error cuando se subio
+
