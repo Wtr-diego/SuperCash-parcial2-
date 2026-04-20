4711a8e1411b3e01a3fd5c166034b1547c51d24c
eduardopin2025 <pa0305032025@unab.edu.sv>
2026-04-19 19:16:26 -0600

    Se agrego detalle venta

---
+++ EL/Detalleventa.cs
@@ -0,0 +1,12 @@
+﻿namespace EL
+{
+    public class DetalleVenta
+    {
+        public int IdDetalle { get; set; }
+        public int IdVenta { get; set; }     // Relación con la Venta
+        public int IdProducto { get; set; }  // Relación con el Producto
+        public int Cantidad { get; set; }    // Cuántos compró
+        public decimal PrecioUnitario { get; set; } // Precio al momento de la venta
+        public decimal Subtotal => Cantidad * PrecioUnitario;
+    }
+}
